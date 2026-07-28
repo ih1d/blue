351f48b6d4b7b4d1ebc46c19dfd160c02ad71e48
Isaac Hiram Lopez Diaz <isaac.lopez@upr.edu>
2026-07-28 02:25:36 -0400

    Change of plans

--- src/Blue/Eval.hs
+++ src/Blue/Eval.hs
@@ -1,6 +1,16 @@
 module Blue.Eval where
 
 import Blue.Core
+import Control.Monad.Trans.State
+import Data.List (nub)
+
+freeVars :: Expr -> [Name]
+freeVars (Var n) = [n]
+freeVars (Lit _) = []
+freeVars (Lam n expr) = [x | x <- freeVars expr, n /= x]
+freeVars (App e0 e1) = nub (freeVars e0 ++ freeVars e1)
+freeVars (Let n e0 e1) = nub (freeVars e0 ++ [x | x <- freeVars e1, n /= x])
+freeVars (Prim _ e0 e1) = nub (freeVars e0 ++ freeVars e1)
 
 eval :: Expr -> Env -> Either Error Value
 eval (Var n) env =
